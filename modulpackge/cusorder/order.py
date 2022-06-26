a395915c8b693bafd3a2d81ecfe6116fd6fbd13b
64210038 <107332146+64210038@users.noreply.github.com>
2022-06-26 15:04:09 +0700

    update

---
+++ modulpackge/cusorder/order.py
@@ -0,0 +1,13 @@
+class order:
+    def __init__(self,dete,satus) -> None:
+        self.dete = dete
+        self.status = status
+
+        def calc_tax(self):
+            pass
+        
+        def cal_total(srlf):
+            pass
+
+        def cal_total_weight(self):
+            pass
